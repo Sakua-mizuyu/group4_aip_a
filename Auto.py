86e922e458b24e2c9192479cb94763bd590d5ff7
gy8810 <56707411+gy8810@users.noreply.github.com>
2020-01-26 17:19:28 +0900

    Sevenメソッドの提出

--- Auto.py
+++ Auto.py
@@ -1,3 +1,5 @@
+import random
+import time
 def check(choose,drinks,M,drinksName):
   if choose in drinks.keys():
       total = drinks[choose]
@@ -9,11 +11,15 @@ def check(choose,drinks,M,drinksName):
           koka=koka+int(money)
           if koka==total:
             print("ちょうどいいです！ご利用いただきありがとうございます。")
+            time.sleep(1.2)
+            Seven()
             break
           elif koka < total:
             print("{}円です。{}円預かりいたしました、{}円足りません。".format(total,koka,total-koka))
           else:
             print("{}円です。{}円預かりいたしました、{}円のお釣りです。ご利用いただきありがとうございます。".format(total,koka,koka-total))
+            time.sleep(1.2)
+            Seven()
             break
         else:
           print("この硬貨または紙幣がありますか？お金をいれてください。")
@@ -33,4 +39,29 @@ def JdohanB():
     "4:コーヒー(130円)\n"
     "Q:quit\n")
     check(choose,drinks,M,drinksName)
+
+def Seven():
+    num1=random.randint(0,9)##本当の確率
+    num2=random.randint(0,8)##表示する数字
+    print("今から抽選します。")
+    time.sleep(0.5)
+    if num1==0:               ##もし乱数は0の時      
+        for i in range(3):
+            print(num2,end=" ")
+            time.sleep(0.8)
+        time.sleep(0.8)
+        print(num2)
+        time.sleep(0.8)
+        print("当たりました！おめでとう！もう一本出します")
+        time.sleep(0.8)
+    else:
+        for i in range(3):
+            print(num2,end=" ")
+            time.sleep(0.8)
+        time.sleep(0.8)
+        print(num2+1)
+        time.sleep(0.8)
+        print("外れてしまいました！残念！")
+        time.sleep(0.8)
 JdohanB()
+
